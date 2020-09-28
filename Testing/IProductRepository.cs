c806615fcf27a2a82dabb21ecebf7d0315453491
Ryan Grayson <ryan.g.grayson@gmail.com>
2020-09-28 14:17:58 -0500

    Added Update Product Link

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -8,6 +8,9 @@ namespace Testing
     {
 
         public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
 
     }
+
 }
